ccace78d2afc4c5de43ce59c1f6ddebdf4620382
felipejulianes <136484753+felipejulianes@users.noreply.github.com>
2025-08-10 16:29:45 -0300

    Create embajadores.js

---
+++ embajadores.js
@@ -0,0 +1,16 @@
+/* embajadores.js — archivo editable por el club */
+window.EMB_LINKS = {
+  "general": {
+    "20000": "https://www.mercadopago.com.ar/subscriptions/checkout?preapproval_plan_id=dceff05f52b94c89a1c7ccedf66d9462",
+    "40000": "https://www.mercadopago.com.ar/subscriptions/checkout?preapproval_plan_id=007bf6ab1fdd48feb687e71bce3c016d",
+    "60000": "https://www.mercadopago.com.ar/subscriptions/checkout?preapproval_plan_id=8660118894b54e009902d035a9000bdb",
+    "libre":  "https://www.mercadopago.com.ar/subscriptions/checkout?preapproval_plan_id=2c9380849817d4a2019818f0477a0055"
+  },
+  "marcos": {
+    "20000": "https://www.mercadopago.com.ar/subscriptions/checkout?preapproval_plan_id=3aed43f7a1494a1fa6727ba95eef432d",
+    "40000": "https://www.mercadopago.com.ar/subscriptions/checkout?preapproval_plan_id=0b987fbc0ff74f88b399b0fb13719863",
+    "60000": "https://www.mercadopago.com.ar/subscriptions/checkout?preapproval_plan_id=655727b76137441eaaf447b372ec049d",
+    "libre":  "https://www.mercadopago.com.ar/subscriptions/checkout?preapproval_plan_id=dceff05f52b94c89a1c7ccedf66d9462"
+  }
+  // Agregá más embajadores acá…
+};
