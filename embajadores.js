04ffadeefd61d19d54c9edcf4b5957666992ec39
felipejulianes <136484753+felipejulianes@users.noreply.github.com>
2025-08-10 17:11:48 -0300

    Update embajadores.js

--- embajadores.js
+++ embajadores.js
@@ -1,4 +1,3 @@
-/* embajadores.js — archivo editable por el club */
 window.EMB_LINKS = {
   "general": {
     "20000": "https://www.mercadopago.com.ar/subscriptions/checkout?preapproval_plan_id=dceff05f52b94c89a1c7ccedf66d9462",
@@ -6,25 +5,33 @@ window.EMB_LINKS = {
     "60000": "https://www.mercadopago.com.ar/subscriptions/checkout?preapproval_plan_id=8660118894b54e009902d035a9000bdb",
     "libre":  "https://www.mercadopago.com.ar/subscriptions/checkout?preapproval_plan_id=2c9380849817d4a2019818f0477a0055"
   },
-  "marcosjulianes": {
+  "marcos": {
+    _label: "Marcos Julianes",
     "20000": "https://www.mercadopago.com.ar/subscriptions/checkout?preapproval_plan_id=3aed43f7a1494a1fa6727ba95eef432d",
     "40000": "https://www.mercadopago.com.ar/subscriptions/checkout?preapproval_plan_id=0b987fbc0ff74f88b399b0fb13719863",
     "60000": "https://www.mercadopago.com.ar/subscriptions/checkout?preapproval_plan_id=655727b76137441eaaf447b372ec049d",
     "libre":  "https://www.mercadopago.com.ar/subscriptions/checkout?preapproval_plan_id=dceff05f52b94c89a1c7ccedf66d9462"
   },
-  "jorgedartidelongue": {
+  "jorge": {
+    _label: "Jorge Dartiguelongue",
     "20000": "https://www.mercadopago.com.ar/subscriptions/checkout?preapproval_plan_id=dceff05f52b94c89a1c7ccedf66d9462",
     "40000": "https://www.mercadopago.com.ar/subscriptions/checkout?preapproval_plan_id=007bf6ab1fdd48feb687e71bce3c016d",
     "60000": "https://www.mercadopago.com.ar/subscriptions/checkout?preapproval_plan_id=8660118894b54e009902d035a9000bdb",
     "libre":  "https://www.mercadopago.com.ar/subscriptions/checkout?preapproval_plan_id=2c9380849817d4a2019818f0477a0055"
   },
-  "carlosramallo": {
+  "carlos": {
+    _label: "Carlos Ramallo",
     "20000": "https://www.mercadopago.com.ar/subscriptions/checkout?preapproval_plan_id=dceff05f52b94c89a1c7ccedf66d9462",
     "40000": "https://www.mercadopago.com.ar/subscriptions/checkout?preapproval_plan_id=007bf6ab1fdd48feb687e71bce3c016d",
     "60000": "https://www.mercadopago.com.ar/subscriptions/checkout?preapproval_plan_id=8660118894b54e009902d035a9000bdb",
     "libre":  "https://www.mercadopago.com.ar/subscriptions/checkout?preapproval_plan_id=2c9380849817d4a2019818f0477a0055"
   },
-  "general": {
+    peta: {
+    _label: "Peta Caride"
+    // Si no ponés montos acá, la redirección usa los links de "general" automáticamente.
+  },
+  "maria": {
+    _label: "Maria Passano",
     "20000": "https://www.mercadopago.com.ar/subscriptions/checkout?preapproval_plan_id=dceff05f52b94c89a1c7ccedf66d9462",
     "40000": "https://www.mercadopago.com.ar/subscriptions/checkout?preapproval_plan_id=007bf6ab1fdd48feb687e71bce3c016d",
     "60000": "https://www.mercadopago.com.ar/subscriptions/checkout?preapproval_plan_id=8660118894b54e009902d035a9000bdb",
